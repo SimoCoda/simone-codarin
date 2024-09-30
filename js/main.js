44c587f294b3b4226d7d5d6bda19467bde57dd36
Simone Codarin <simone.codarin@cgsi.it>
2024-09-30 14:38:21 +0200

    ottimmizzato lo scroll con il go-top, minimizzato il js e importato

--- js/main.js
+++ js/main.js
@@ -288,18 +288,11 @@
   /* Back to top
    ------------------------------------------------------- */
   var pxShow = 300; // height on which the button will show
-  var fadeInTime = 400; // how slow/fast you want the button to show
-  var fadeOutTime = 400; // how slow/fast you want the button to hide
-  var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
-
-  // Show or hide the sticky footer button
-  jQuery(window).scroll(function () {
-    if (!$("#header-search").hasClass("is-visible")) {
-      if (jQuery(window).scrollTop() >= pxShow) {
-        jQuery("#go-top").fadeIn(fadeInTime);
-      } else {
-        jQuery("#go-top").fadeOut(fadeOutTime);
-      }
+  window.addEventListener('scroll', function() {
+    if (window.scrollY >= pxShow) {
+      document.getElementById("go-top").style.display = 'block';
+    } else {
+      document.getElementById("go-top").style.display = 'none';
     }
   });
 })(jQuery);
